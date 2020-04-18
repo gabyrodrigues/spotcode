303786b4590727bba91169c98b2a3fea74876593
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-04-17 23:41:06 -0300

    página discovery + página de album (album/:id)

--- app/javascript/screens/album/index.js
+++ app/javascript/screens/album/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import NavbarFooter from '../../components/common/NavbarFooter';
+import Albums from '../../components/Albums';
+import SectionWrapper from '../../components/common/SectionWrapper';
 
 const AlbumScreen = () => {
-    return (
-        <>
-            <div>Album screen</div>
-            <NavbarFooter />
-        </>
-    );
+	return (
+		<>
+			<SectionWrapper>
+				<Albums/>
+				<NavbarFooter/>
+			</SectionWrapper>
+		</>
+	);
 }
 
 export default AlbumScreen;
--- app/javascript/services/albums.js
+++ app/javascript/services/albums.js
@@ -1,7 +1,8 @@
 import api from './api';
  
 const AlbumsService = {
-  index: () => api.get('/dashboard')
+  index: () => api.get('/dashboard'),
+  show: (id) => api.get(`/albums/${id}`)
 }
  
 export default AlbumsService;
